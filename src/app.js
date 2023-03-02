ba2df542aa0926d3cdf5b3398dcf8387083151df
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-03-01 20:48:32 -0500

    Ready to start project

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ let databaseLowerCase = [];
 database.map((value, index) => {
   databaseLowerCase.push(value.toLowerCase())
 })
+let result = []
+
 console.log(databaseLowerCase);
 app.get('/', function (req, res) {
     //get requests retrieve, post requests send.
@@ -19,9 +21,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
   // console.log(req.body);
   let names = res.req.query; //we saved query object as its own variable for get requests in forms
+
   console.log(names.search);
   let searchQuery = names.search;
-  let result = databaseLowerCase.filter( results => results.includes(searchQuery.toLowerCase()))
+  //basically have this logic print on the html page
+  if(searchQuery){
+    result = databaseLowerCase.filter(results => results.includes(searchQuery.toLowerCase()))
+  }else{
+    console.log(databaseLowerCase);
+  }
   console.log(result)
   //database filtered by search query
 
