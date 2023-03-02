5e0df415b130b1394d22714184dd6d1a91b1567a
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-03-01 20:18:30 -0500

    First Commit Assignment 1

--- src/app.js
+++ src/app.js
@@ -6,15 +6,32 @@ const app = express()
 
 const path = require("path")
 
+let database = ["Sophia", "Jackson", "Olivia", "Liam", "Emma", "Noah", "Ava", "Aiden", "Isabella", "Caden", "Mia", "Grayson", "Charlotte", "Elijah", "Amelia"];
+let databaseLowerCase = [];
+database.map((value, index) => {
+  databaseLowerCase.push(value.toLowerCase())
+})
+console.log(databaseLowerCase);
 app.get('/', function (req, res) {
     //get requests retrieve, post requests send.
     //we "req" (request) /home, we "res" (receive) "Hello Home"
   //Instead of 'hello home', we could send the html file
   res.sendFile(path.join(__dirname, '/index.html'));
+  // console.log(req.body);
+  let names = res.req.query; //we saved query object as its own variable for get requests in forms
+  console.log(names.search);
+  let searchQuery = names.search;
+  let result = databaseLowerCase.filter( results => results.includes(searchQuery.toLowerCase()))
+  console.log(result)
+  //database filtered by search query
+
+  //useful for putting data into a database in future
 })
 
 app.get('/apartment', function (req, res) {
-    res.send('Hello apartment')
+  res.sendFile(path.join(__dirname, '/index.html'));
+  
   })
 
 app.listen(3000)
+
