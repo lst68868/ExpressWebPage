6c9caada2feb34c89e6a5762cd82fcbffd15b58f
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-03-03 15:00:30 -0500

    saving changes 3/3/23

---
+++ app.js
@@ -0,0 +1,45 @@
+////////IMPORTS/////////
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname,'./views'));
+/////////////////////////
+
+///Search Button Logic///
+
+let names = [  "emma",  "olivia",  "ava",  "isabella",  "sophia",  "charlotte",  "mia",  "amelia",  "harper",  "evelyn",  "abigail",  "emily",  "elizabeth",  "avery",  "ella",  "scarlett",  "grace",  "chloe",  "victoria",  "aubrey"];
+
+let searchText = "";
+//request and response
+//each of the "app.get" thingies is called a "route"
+app.get('/', (req, res) => {
+    let num = 5
+    res.render('home', {num});
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/array', (req, res) => {
+    // searchText = req.query["searchText"]
+    req.query["searchText"];
+    if(req.query["searchText"] !== undefined){
+        
+        searchText = req.query["searchText"].toLowerCase();
+    }
+    let nameFilter = names.filter(search => search.includes(searchText));
+    
+    res.render('array', {nameFilter});
+
+
+});
+
+//create a forEach loop that iterates over every item in nameFilter and makes it an item in a bulleted list.
+
+app.listen(3000);
